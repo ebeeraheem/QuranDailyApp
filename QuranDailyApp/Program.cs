3fa281e6daee46c5c0c002ecb0740ffc771a4dcd
Ibrahim Suleiman <ebeeraheem@gmail.com>
2026-02-02 10:05:21 +0100

    Refactor: move models/services to QuranDailyApp.Core
    Moved core models and QuranService from QuranDailyApp to new QuranDailyApp.Core project. Updated references throughout the app to use QuranDailyApp.Core namespaces. Improves code organization and reusability by centralizing shared logic in a dedicated core library.

--- QuranDailyApp/Program.cs
+++ QuranDailyApp/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using QuranDailyApp;
-using QuranDailyApp.Services;
+using QuranDailyApp.Core.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
